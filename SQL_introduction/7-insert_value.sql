57a785a2aa04927d19596ad94e15027780abf6ce
unknown <7060@holbertonstudents.com>
2023-12-10 14:38:37 -0500

    SQL - Introduction

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- 0x0D. SQL - Introduction, task 7. First add
+-- Inserts new row into `first_table`, DB passed as script arg.
+INSERT INTO first_table (id, name)
+       VALUES (89, 'Holberton School');
