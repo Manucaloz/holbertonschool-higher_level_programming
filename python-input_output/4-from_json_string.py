cf269373422f27a4296db3c49f0e5282fd0987c8
unknown <7060@holbertonstudents.com>
2023-12-17 18:29:34 -0500

    input output

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import json
+
+
+def from_json_string(my_str):
+    """Return the object represented my my_str.
+    Args:
+        - my_str: JSON string representation
+    Returns: corresponding object
+    """
+
+    return json.loads(my_str)
