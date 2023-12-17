6b50960877e2d5d85e25ef358a515bed733fa5f2
unknown <7060@holbertonstudents.com>
2023-12-17 18:14:27 -0500

    inheritance

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""MyList module.
-
-Contains a class MyList that inherits from list
-and a method that prints the sorted list.
-"""
-
 
 class MyList(list):
     """Defines the MyList class."""
